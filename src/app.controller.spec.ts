ce91f46001e6b4fa14d0a3bac07cf2b3b07a43d3
Dave <david@mapli.co.za>
2025-11-01 11:55:22 +0200

    Fix tests to match actual app response

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello world from Dave :)"', () => {
+      expect(appController.getHello()).toBe('Hello world from Dave :)');
     });
   });
 });

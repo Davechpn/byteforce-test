3f20b212f2f93f7f737afe87cf5064c254b8a922
Dave <david@mapli.co.za>
2025-11-01 11:53:05 +0200

    Ready for deployment

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from dave!';
+    return 'Hello world from Dave :)';
   }
 }
